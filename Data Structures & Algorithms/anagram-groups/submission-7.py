545488cf6a0713dd0089b235119da41b6ed7b8af
AtunBetun <78117649+AtunBetun@users.noreply.github.com>
2026-06-08 18:11:17 -0600

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+import json
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #
+        def get_anagram_type(curr: str) -> str:
+            count = Counter(curr)
+            count = dict(sorted(count.items(), key=lambda item: item[0]))
+            print(f"{curr=} {count=}")
+            return json.dumps(count)
+        
+        # 1. master dict of "".join of a dicts value -> anagram list -> dict of []
+        ans = defaultdict(list)
+        # Algo
+        for curr_str in strs:
+            ana = get_anagram_type(curr_str)
+            ans[ana].append(curr_str)
+        return list(ans.values())
+        
+
