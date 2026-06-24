e4b6261a00a42fc7b9c26dd6a56f4f92f3a71a61
AtunBetun <78117649+AtunBetun@users.noreply.github.com>
2026-06-24 12:06:45 -0600

    Add: minimum-window-with-characters - submission-27

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-27.py
@@ -0,0 +1,20 @@
+from collections import deque, defaultdict
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        q = deque([])
+        t_count = Counter(t)
+        curr_count = Counter()
+        ans = ""
+        for r in range(len(s)):
+            q.append(s[r])
+            curr_count[s[r]] += 1
+            # print(f"{q=} {ans=}")
+            while q and curr_count >= t_count: # found answer, start shrinking
+                curr_ans = "".join(q)
+                # print(f"FOUND: {curr_ans=}")
+                if len(curr_ans) < len(ans) or ans == "":
+                    print(f"NEW_MIN: {curr_ans=}")
+                    ans = curr_ans
+                popped = q.popleft()
+                curr_count[popped] -= 1
+        return ans
